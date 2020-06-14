8ca88ac70b4805b221105912435e0bee9e0ddf9e
gunan <65609856+Abdullahgnan@users.noreply.github.com>
2020-06-15 02:47:11 +0300

    Otomasyon Ödevi
    Abdullah Günan

---
+++ src/ParkOtomasyonu/OtoparkOtomasyon.java
@@ -0,0 +1,11 @@
+
+package ParkOtomasyonu;
+
+public class OtoparkOtomasyon {
+
+    public static void main(String[] args) {
+       ÜyeGiriþi ug = new ÜyeGiriþi();
+       ug.setVisible(true);
+    }
+    
+}
